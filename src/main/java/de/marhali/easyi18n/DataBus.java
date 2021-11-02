2b36b355e4069b076adf400c97351b204e82ad34
Marcel Haßlinger <me@marhali.de>
2021-11-02 16:43:13 +0100

    introduce new event system bus

---
+++ src/main/java/de/marhali/easyi18n/DataBus.java
@@ -0,0 +1,55 @@
+package de.marhali.easyi18n;
+
+import de.marhali.easyi18n.model.BusListener;
+import de.marhali.easyi18n.model.TranslationData;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Data-bus which is used to distribute changes regarding translations or ui tools to the participating components.
+ * @author marhali
+ */
+public class DataBus {
+
+    private final Set<BusListener> listener;
+
+    protected DataBus() {
+        this.listener = new HashSet<>();
+    }
+
+    /**
+     * Adds a participant to the event bus. Every participant needs to be added manually.
+     * @param listener Bus listener
+     */
+    public void addListener(BusListener listener) {
+        this.listener.add(listener);
+    }
+
+    /**
+     * Fires the called events on the returned prototype.
+     * The event will be distributed to all participants which were registered at execution time.
+     * @return Listener prototype
+     */
+    public BusListener propagate() {
+        return new BusListener() {
+            @Override
+            public void onUpdateData(@NotNull TranslationData data) {
+                listener.forEach(li -> li.onUpdateData(data));
+            }
+
+            @Override
+            public void onFocusKey(@Nullable String key) {
+                listener.forEach(li -> li.onFocusKey(key));
+            }
+
+            @Override
+            public void onSearchQuery(@Nullable String query) {
+                listener.forEach(li -> li.onSearchQuery(query));
+            }
+        };
+    }
+}
