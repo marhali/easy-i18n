ddebb04871ce1f364e6e506b23526f1c284bb8e9
marhali <me@marhali.de>
2022-04-10 13:00:48 +0200

    remove legacy structure

--- src/main/java/de/marhali/easyi18n/model/translation/variant/PluralMap.java
+++
@@ -1,9 +0,0 @@
-package de.marhali.easyi18n.model.translation.variant;
-
-import java.util.HashMap;
-
-/**
- * Maps pluralization with locale values.
- * @author marhali
- */
-public class PluralMap extends HashMap<Plural, LocaleMap> {}
