c56812c888cf85be86371394ba5f1d48ded1023e
JPilson <joaosumbo34@gmail.com>
2023-12-16 17:51:29 +0100

    Implement selectable text replacement with i18n keys
    A new feature is introduced to replace the selected text in editor with Internationalization (i18n) keys, improving the coding workflow for developers handling string localization. The i18n keys are generated based on the customized settings or default flavor template. Further, a function to retrieve the key field from the `TranslationDialog` is added and a new utility class `DocumentUtil` for checking document file types has been added.

--- src/main/java/de/marhali/easyi18n/action/LocalizeItAction.java
+++ src/main/java/de/marhali/easyi18n/action/LocalizeItAction.java
@@ -4,10 +4,15 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import de.marhali.easyi18n.dialog.AddDialog;
 import de.marhali.easyi18n.model.KeyPath;
+import de.marhali.easyi18n.settings.ProjectSettingsService;
+import de.marhali.easyi18n.util.DocumentUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -26,18 +31,40 @@ class LocalizeItAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
         DataContext dataContext = anActionEvent.getDataContext();
         Editor editor = CommonDataKeys.EDITOR.getData(dataContext);
-        if (editor == null)
-            return;
+        if (editor == null) return;
         String text = editor.getSelectionModel().getSelectedText();
-        if (text == null || text.isEmpty())
-            return;
 
+        if (text == null || text.isEmpty()) return;
+
+
+        if ((text.startsWith("\"") && text.endsWith("\"")) || (text.startsWith("'") && text.endsWith("'"))) {
+            text = text.substring(1);
+            text = text.substring(0, text.length() - 1);
+
+        }
         Project project = anActionEvent.getProject();
         if (project == null) {
             throw new RuntimeException("Project is null!");
         }
 
-        AddDialog dialog = new AddDialog(project, new KeyPath(text), text);
+        AddDialog dialog = new AddDialog(project, new KeyPath(text), text, (key) -> replaceSelectedText(project, editor, key));
         dialog.showAndHandle();
+
+
+    }
+
+    private void replaceSelectedText(Project project, @NotNull Editor editor, @NotNull String key) {
+        int selectionStart = editor.getSelectionModel().getSelectionStart();
+        int selectionEnd = editor.getSelectionModel().getSelectionEnd();
+        String flavorTemplate = ProjectSettingsService.get(project).getState().getFlavorTemplate();
+        DocumentUtil documentUtil = new DocumentUtil(editor.getDocument());
+        String replacement = buildReplacement(flavorTemplate, key, documentUtil);
+        WriteCommandAction.runWriteCommandAction(editor.getProject(), () -> documentUtil.getDocument().replaceString(selectionStart, selectionEnd, replacement));
+    }
+
+    private String buildReplacement(String flavorTemplate, String key, DocumentUtil documentUtil) {
+        if (documentUtil.isVue() || documentUtil.isJsOrTs()) return flavorTemplate + "('" + key + "')";
+
+        return flavorTemplate + "(\"" + key + "\")";
     }
 }
