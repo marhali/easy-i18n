1021855bcfffeeac1256c3caf1736e1abd86b158
marhali <code@shield.marhali.de>
2025-12-01 23:12:17 +0100

    refactor: outsource module association

--- src/main/java/de/marhali/easyi18n/next_io/I18nFile.java
+++ src/main/java/de/marhali/easyi18n/next_io/I18nFile.java
@@ -1,7 +1,6 @@
 package de.marhali.easyi18n.next_io;
 
 import com.intellij.openapi.vfs.VirtualFile;
-import de.marhali.easyi18n.config.project.ProjectConfigModule;
 
 import java.util.Map;
 
@@ -9,18 +8,12 @@ import java.util.Map;
  * @author marhali
  */
 public class I18nFile {
-    private final ProjectConfigModule module;
     private final VirtualFile file;
     private final Map<String, String> params;
 
-    public I18nFile(ProjectConfigModule module, VirtualFile file, Map<String, String> params) {
-        this.module = module;
+    public I18nFile(VirtualFile file, Map<String, String> params) {
         this.file = file;
         this.params = params;
-    }
-
-    public ProjectConfigModule getModule() {
-        return module;
     }
 
     public VirtualFile getFile() {
@@ -34,8 +27,7 @@ public class I18nFile {
     @Override
     public String toString() {
         return "I18nFile{" +
-            "module=" + module +
-            ", file=" + file +
+            "file=" + file +
             ", params=" + params +
             '}';
     }
