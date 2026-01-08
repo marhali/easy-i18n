23e5776aacbf2bf2dba7dd18eff9c54522ab5020
marhali <code@shield.marhali.de>
2026-01-08 23:26:21 +0100

    temp: trigger new io ops via reload action

--- src/main/java/de/marhali/easyi18n/action/ReloadAction.java
+++ src/main/java/de/marhali/easyi18n/action/ReloadAction.java
@@ -4,11 +4,10 @@ import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
-import de.marhali.easyi18n.InstanceManager;
-
+import de.marhali.easyi18n.config.project.ProjectConfigService;
+import de.marhali.easyi18n.next_io.PersistenceHandler;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
@@ -24,6 +23,17 @@ public class ReloadAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        InstanceManager.get(Objects.requireNonNull(e.getProject())).reload();
+        //InstanceManager.get(Objects.requireNonNull(e.getProject())).reload();
+        // Just a small button to trigger our new IO system
+
+        var persi = new PersistenceHandler(e.getProject(), ProjectConfigService.forProject(e.getProject()).getState());
+        try {
+            var store = persi.read();
+            System.out.println(store);
+            System.out.println("----");
+            persi.write(store);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
