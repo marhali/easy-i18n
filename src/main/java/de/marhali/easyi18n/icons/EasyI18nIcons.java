810f6a777de4e690cf788de8bc40fd18a6075168
marhali <code@shield.marhali.de>
2026-01-08 23:07:26 +0100

    chore: demonstrate use of custom icons

---
+++ src/main/java/de/marhali/easyi18n/icons/EasyI18nIcons.java
@@ -0,0 +1,9 @@
+package de.marhali.easyi18n.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface EasyI18nIcons {
+    Icon TRANSLATE_ICON = IconLoader.getIcon("/icons/translate13.svg", EasyI18nIcons.class);
+}
