7057621150665121ef980cf6e1bdc8c51a9695a3
marhali <code@shield.marhali.de>
2026-02-02 23:51:20 +0100

    feat: implement intellij DomainEventPublisherAdapter

---
+++ src/main/java/de/marhali/easyi18n/idea/event/DomainEventPublisherAdapter.java
@@ -0,0 +1,26 @@
+package de.marhali.easyi18n.idea.event;
+
+import com.intellij.openapi.project.Project;
+import de.marhali.easyi18n.core.domain.event.DomainEvent;
+import de.marhali.easyi18n.core.domain.model.ProjectId;
+import de.marhali.easyi18n.core.ports.DomainEventPublisherPort;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Adapter to connect the {@link DomainEventPublisherPort} with IntelliJ's {@link com.intellij.util.messages.MessageBus}.
+ *
+ * @author marhali
+ */
+public class DomainEventPublisherAdapter implements DomainEventPublisherPort {
+
+    private final @NotNull Project project;
+
+    public DomainEventPublisherAdapter(@NotNull Project project) {
+        this.project = project;
+    }
+
+    @Override
+    public void publish(@NotNull ProjectId projectId, @NotNull DomainEvent event) {
+        project.getMessageBus().syncPublisher(PluginTopics.DOMAIN_EVENTS).onDomainEvent(event);
+    }
+}
---
+++ src/main/java/de/marhali/easyi18n/idea/event/PluginTopics.java
@@ -0,0 +1,18 @@
+package de.marhali.easyi18n.idea.event;
+
+import com.intellij.util.messages.Topic;
+import de.marhali.easyi18n.core.domain.event.DomainEvent;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Plugin related message topics that are placed on IntelliJ's {@link com.intellij.util.messages.MessageBus}.
+ *
+ * @author marhali
+ */
+public interface PluginTopics {
+    Topic<DomainListener> DOMAIN_EVENTS = Topic.create("Domain Events", DomainListener.class);
+
+    interface DomainListener {
+        void onDomainEvent(@NotNull DomainEvent event);
+    }
+}
