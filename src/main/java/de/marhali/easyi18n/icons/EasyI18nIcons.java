8f46e7a0752a90f27803375babafc7e4d93e777a
marhali <code@shield.marhali.de>
2026-02-02 23:00:53 +0100

    refactor: rename icon registry

--- src/main/java/de/marhali/easyi18n/icons/EasyI18nIcons.java
+++
@@ -1,9 +0,0 @@
-package de.marhali.easyi18n.icons;
-
-import com.intellij.openapi.util.IconLoader;
-
-import javax.swing.*;
-
-public interface EasyI18nIcons {
-    Icon TRANSLATE_ICON = IconLoader.getIcon("/icons/translate13.svg", EasyI18nIcons.class);
-}
---
+++ src/main/java/de/marhali/easyi18n/idea/icons/PluginIcon.java
@@ -0,0 +1,9 @@
+package de.marhali.easyi18n.idea.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface PluginIcon {
+    Icon SHOW_AS_TREE_ICON = IconLoader.getIcon("/icons/showAsTree.svg", PluginIcon.class);
+}
