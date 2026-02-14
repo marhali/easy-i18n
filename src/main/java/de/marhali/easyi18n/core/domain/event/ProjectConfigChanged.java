bfa09fe8cfe610327c7e02c4cd3d8f1b64b1221a
marhali <code@shield.marhali.de>
2026-02-14 18:42:58 +0100

    chore: add author

--- src/main/java/de/marhali/easyi18n/core/domain/event/ProjectConfigChanged.java
+++ src/main/java/de/marhali/easyi18n/core/domain/event/ProjectConfigChanged.java
@@ -3,6 +3,7 @@ package de.marhali.easyi18n.core.domain.event;
 /**
  * Event that is published when the configuration of the project has been changed.
  *
+ * @author marhali
  */
 public record ProjectConfigChanged() implements DomainEvent {
 }
