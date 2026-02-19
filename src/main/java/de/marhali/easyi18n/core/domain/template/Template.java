7f3c58dda95068c8033a71ba220d2cc28f5f95c7
marhali <code@shield.marhali.de>
2026-02-19 23:31:29 +0100

    feat: implement Template record

---
+++ src/main/java/de/marhali/easyi18n/core/domain/template/Template.java
@@ -0,0 +1,17 @@
+package de.marhali.easyi18n.core.domain.template;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+/**
+ * Represents a parsed template.
+ *
+ * @param canonical Template syntax
+ * @param elements Parsed template elements
+ */
+public record Template(
+    @NotNull String canonical,
+    @NotNull List<@NotNull TemplateElement> elements
+    ) {
+}
