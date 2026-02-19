b1357a8a8a78f2371f63f835ad64b864924d40e7
marhali <code@shield.marhali.de>
2026-02-19 22:20:47 +0100

    feat: add from function to take Map.Entry entries

--- src/main/java/de/marhali/easyi18n/core/domain/model/I18nEntry.java
+++ src/main/java/de/marhali/easyi18n/core/domain/model/I18nEntry.java
@@ -2,6 +2,8 @@ package de.marhali.easyi18n.core.domain.model;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
+
 /**
  * Single translation entry.
  *
@@ -14,4 +16,7 @@ public record I18nEntry(
     @NotNull I18nKey key,
     @NotNull I18nContent content
 ) {
+    public static @NotNull I18nEntry fromEntry(@NotNull Map.Entry<@NotNull I18nKey, @NotNull I18nContent> entry) {
+        return new I18nEntry(entry.getKey(), entry.getValue());
+    }
 }
