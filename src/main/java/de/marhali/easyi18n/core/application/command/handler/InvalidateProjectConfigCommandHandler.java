dad8e1b4f9b49d9a7b643d9dac835168937e0516
marhali <code@shield.marhali.de>
2026-02-19 23:43:25 +0100

    fix: use correct spelling

--- src/main/java/de/marhali/easyi18n/core/application/command/handler/InvalidateProjectConfigCommandHandler.java
+++ src/main/java/de/marhali/easyi18n/core/application/command/handler/InvalidateProjectConfigCommandHandler.java
@@ -2,7 +2,7 @@ package de.marhali.easyi18n.core.application.command.handler;
 
 import de.marhali.easyi18n.core.application.command.InvalidateProjectConfigCommand;
 import de.marhali.easyi18n.core.application.cqrs.CommandHandler;
-import de.marhali.easyi18n.core.application.service.CachedModuleTamplates;
+import de.marhali.easyi18n.core.application.service.CachedModuleTemplates;
 import de.marhali.easyi18n.core.application.state.I18nStore;
 import de.marhali.easyi18n.core.domain.event.ProjectConfigChanged;
 import de.marhali.easyi18n.core.domain.model.MutableI18nProject;
@@ -12,19 +12,19 @@ import org.jetbrains.annotations.NotNull;
 public class InvalidateProjectConfigCommandHandler implements CommandHandler<InvalidateProjectConfigCommand> {
 
     private final @NotNull I18nStore store;
-    private final @NotNull CachedModuleTamplates cachedModuleTamplates;
+    private final @NotNull CachedModuleTemplates cachedModuleTemplates;
     private final @NotNull DomainEventPublisherPort domainEventPublisherPort;
 
-    public InvalidateProjectConfigCommandHandler(@NotNull I18nStore store, @NotNull CachedModuleTamplates cachedModuleTamplates, @NotNull DomainEventPublisherPort domainEventPublisherPort) {
+    public InvalidateProjectConfigCommandHandler(@NotNull I18nStore store, @NotNull CachedModuleTemplates cachedModuleTemplates, @NotNull DomainEventPublisherPort domainEventPublisherPort) {
         this.store = store;
-        this.cachedModuleTamplates = cachedModuleTamplates;
+        this.cachedModuleTemplates = cachedModuleTemplates;
         this.domainEventPublisherPort = domainEventPublisherPort;
     }
 
     @Override
     public void handle(@NotNull InvalidateProjectConfigCommand command) {
         store.mutate(MutableI18nProject::clear);
-        cachedModuleTamplates.invalidateAll();
+        cachedModuleTemplates.invalidateAll();
         domainEventPublisherPort.publish(new ProjectConfigChanged());
     }
 }
