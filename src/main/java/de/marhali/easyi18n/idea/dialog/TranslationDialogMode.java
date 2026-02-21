12cdd0985a26fd59968e2f1b0b00d30970c966bd
marhali <code@shield.marhali.de>
2026-02-21 19:47:15 +0100

    feat: define translation dialog modes

---
+++ src/main/java/de/marhali/easyi18n/idea/dialog/TranslationDialogMode.java
@@ -0,0 +1,19 @@
+package de.marhali.easyi18n.idea.dialog;
+
+/**
+ * Enumeration of translation dialog modes.
+ *
+ * @author marhali
+ */
+public enum TranslationDialogMode {
+    /**
+     * Add a new translation.
+     */
+    ADD,
+
+    /**
+     * Edit an existing translation.
+     */
+    EDIT,
+    ;
+}
