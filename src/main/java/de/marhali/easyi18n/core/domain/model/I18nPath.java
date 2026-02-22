659472e9ee494d8fcd97e5571071f5e16c09bcfe
marhali <code@shield.marhali.de>
2026-02-22 23:54:22 +0100

    feat: add shorthand constructor

--- src/main/java/de/marhali/easyi18n/core/domain/model/I18nPath.java
+++ src/main/java/de/marhali/easyi18n/core/domain/model/I18nPath.java
@@ -1,5 +1,6 @@
 package de.marhali.easyi18n.core.domain.model;
 
+import de.marhali.easyi18n.core.domain.template.TemplateValue;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -14,4 +15,12 @@ public record I18nPath(
     @NotNull String canonical,
     @NotNull I18nParams params
 ) {
+    /**
+     * Shorthand to construct a translation file path from a built template value.
+     * @param templateValue Built template value
+     * @return {@link I18nPath}
+     */
+    public static @NotNull I18nPath fromTemplateValue(@NotNull TemplateValue templateValue) {
+        return new I18nPath(templateValue.value(), templateValue.params());
+    }
 }
