b2c1ca3ea351c6e3b110d1837e902757f236c352
marhali <code@shield.marhali.de>
2026-03-11 23:39:27 +0100

    feat: add translate icon

--- src/main/java/de/marhali/easyi18n/idea/icons/PluginIcon.java
+++ src/main/java/de/marhali/easyi18n/idea/icons/PluginIcon.java
@@ -5,5 +5,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface PluginIcon {
+    Icon TRANSLATE_ICON = IconLoader.getIcon("/icons/translate13.svg", PluginIcon.class);
     Icon SHOW_AS_TREE_ICON = IconLoader.getIcon("/icons/showAsTree.svg", PluginIcon.class);
 }
