6b085339a711896df980ae51c92b6c55772b3c3e
marhali <code@shield.marhali.de>
2026-03-12 19:12:57 +0100

    chore: add docs

--- src/main/java/de/marhali/easyi18n/idea/icons/PluginIcon.java
+++ src/main/java/de/marhali/easyi18n/idea/icons/PluginIcon.java
@@ -4,6 +4,11 @@ import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
+/**
+ * Plugin specific icons.
+ *
+ * @author marhali
+ */
 public interface PluginIcon {
     Icon TRANSLATE_ICON = IconLoader.getIcon("/icons/translate13.svg", PluginIcon.class);
     Icon SHOW_AS_TREE_ICON = IconLoader.getIcon("/icons/showAsTree.svg", PluginIcon.class);
